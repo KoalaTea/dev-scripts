06cb6cdb8d515069c962e3ad6fb210cdcf4865ab
koalatea <koalateac@gmail.com>
2019-09-26 18:23:09 -0500

    vscode haskell

--- vscode.sh
+++ vscode.sh
@@ -4,4 +4,5 @@ code --install-extension ryanluker.vscode-coverage-gutters && \
 code --install-extension peterjausovec.vscode-docker && \
 code --install-extension dbaeumer.vscode-eslint && \
 code --install-extension esbenp.prettier-vscode && \
-code --install-extension fb-pyre-check.pyre-vscode
+code --install-extension fb-pyre-check.pyre-vscode && \
+code --install-extension justusadam.language-haskell
