6b680c52c5412f84a659d47f0c30eee343f82a1c
koalatea <koalateac@gmail.com>
2019-09-26 18:21:14 -0500

    default vscode extensions

---
+++ vscode.sh
@@ -0,0 +1,7 @@
+code --install-extension ms-python.python && \
+code --install-extension ms-vscode.go && \
+code --install-extension ryanluker.vscode-coverage-gutters && \
+code --install-extension peterjausovec.vscode-docker && \
+code --install-extension dbaeumer.vscode-eslint && \
+code --install-extension esbenp.prettier-vscode && \
+code --install-extension fb-pyre-check.pyre-vscode
